b5d549b401898b4f7c1e0db05545efdb0e0f5615
Yeimi Moreno <ymoreno@twilio.com>
2020-08-19 12:57:31 -0500

    Remove api key and secret from readme and config because they were not used anymore

--- config/index.js
+++ config/index.js
@@ -3,8 +3,6 @@ require("dotenv").config();
 module.exports = {
   TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
   TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
-  TWILIO_API_KEY: process.env.TWILIO_API_KEY,
-  TWILIO_API_SECRET: process.env.TWILIO_API_SECRET,
   TWILIO_VERIFY_SERVICE_SID: process.env.TWILIO_VERIFY_SERVICE_SID,
   HASH_IDENTITY: (process.env.HASH_IDENTITY || "true") === "true",
   BACKEND_BASE_URL: process.env.BACKEND_BASE_URL
